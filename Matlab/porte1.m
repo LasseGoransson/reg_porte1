7f2bdc34bb5a25e4dd82f81afc1ff803602b28cb
Anne Poulsen <36919672+AC12344@users.noreply.github.com>
2019-03-28 17:36:05 +0100

    commit to pull. includes the matlab files and simulink files

--- Matlab/porte1.m
+++ Matlab/porte1.m
@@ -8,7 +8,7 @@ lp = pi/3;
 I = 1/3*m*((2*l)^2);
 b = 0.1;
 
-% State space model / Linearized at pi/3
+%% State space model / Linearized at pi/3
 
 A = [0 1; (g*m*l*cos(lp))/I -b/I];
 B = [0; 1/I];
@@ -19,7 +19,7 @@ openloop_ss = ss(A,B,C,D);
 [gs_a, gs_b] = ss2tf(A,B,C,D);
 openloop_tf = tf(gs_a, gs_b);
 
-%Specification
+%%Specification
 
 tr = 1.0;
 ts = 1.2;
@@ -35,10 +35,80 @@ sigma = 4.6/ts
 % Regulering
 s = tf('s');
 
-kp=0;%2.46;
-ki=0;
-kd=0;
+% Ks og nulpunkter
+Td = 1/6.86;
+Ti = 1/1.66;
+kp = 40.86;
+Ks = kp*(Td*s^2+s+1/Ti)/s
 
-ks = kp + ki/s + s*kd;
+%karakteristisk ligning
+Ls = Ks*Gs % openloop
 
-cltf = ks*openloop_tf/(1+ks*openloop_tf);
+rlocus(Ls)
+
+
+Ts = (Ks*Gs)/(1+Ks*Gs) % closedloop
+
+step(Ts)
+
+stepinfo(Ts)
+
+% n;ste fors'g med nye nulpunkter
+% Ks og nulpunkter
+Td = 1/6.86;
+Ti = 1/1.66;
+kp = 60.86;
+Ks = kp*(Td*s^2+s+1/Ti)/s
+
+%karakteristisk ligning
+Ls = Ks*Gs % openloop
+
+Ls = Ks*Gs % openloop
+
+rlocus(Ls)
+
+Ts = (Ks*Gs)/(1+Ks*Gs) % closedloop
+
+step(Ts)
+
+stepinfo(Ts)
+
+% n;ste fors'g med nye nulpunkter
+% Ks og nulpunkter
+Td = 1/2;
+Ti = 1/10;
+kp = 21.86;
+Ks = kp*(Td*s^2+s+1/Ti)/s
+
+Ls = Ks*Gs % openloop
+
+rlocus(Ls)
+Ts = (Ks*Gs)/(1+Ks*Gs) % closedloop
+
+pzmap(Ts)
+step(Ts)
+
+stepinfo(Ts)
+
+Td = 1/6.86;
+Ti = 1/1.66;
+kp = 60.86;
+
+Kd = Td*kp
+Ki = (1/Ti)*kp
+
+ks = kp*(Td*s^2+s+1/Ti)/s;
+ks_simu= kp + Ki*(1/s)+Kd*s;
+
+cltf = ks_simu*openloop_tf/(1+ks*openloop_tf);
+
+clft_1= ks*openloop_tf/(1+ks*openloop_tf);
+figure(1)
+step(cltf)
+stepinfo(cltf)
+
+figure(2)
+step(clft_1)
+stepinfo(clft_1)
+
+
